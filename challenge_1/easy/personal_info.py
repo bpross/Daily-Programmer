61eaddbb3c558bdc1fdef8ccacd8fe31b593ab51
Benjamin Ross <benr22@gmail.com>
2015-09-30 20:56:51 -0700

    First easy challenge complete

---
+++ challenge_1/easy/personal_info.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+name = raw_input("What is your name? ")
+age = raw_input("What is your age? ")
+user_name = raw_input("What is your reddit user name? ")
+
+print "Your name is {}, you are {} years old, and your reddit username is: {}".format(name, age, user_name)
