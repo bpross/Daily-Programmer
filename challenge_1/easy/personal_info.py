3bcf0a87f2969b424f4360966cb631ba32881925
Benjamin Ross <benr22@gmail.com>
2015-09-30 20:59:29 -0700

    Added printing to a file for extra credit

--- challenge_1/easy/personal_info.py
+++ challenge_1/easy/personal_info.py
@@ -4,4 +4,11 @@ name = raw_input("What is your name? ")
 age = raw_input("What is your age? ")
 user_name = raw_input("What is your reddit user name? ")
 
-print "Your name is {}, you are {} years old, and your reddit username is: {}".format(name, age, user_name)
+info = "Your name is {}, you are {} years old, and your reddit username is: {}".format(name, age, user_name)
+print info
+
+target = open("user_info.txt", 'w')
+
+target.write(info)
+target.write("\n")
+target.close()
